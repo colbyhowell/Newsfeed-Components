2b874e0c0a79093dbb1a461c0d14f3e25ebd2de8
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-07-16 20:33:07 -0500

    finished the initial html build in JS

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,21 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+
+const articleList = document.querySelector('.articles')
+
+function createArticle(title, date, firstParagraph, secondParagraph, thirdParagraph){
+  const article = document.createElement('div')
+  const articleTitle = document.createElement('h2')
+  const articleDate = document.createElement('p')
+  const articleContent = document.createElement('p')
+  const articleExpand = document.createElement('span')
+
+    article.appendChild(articleTitle)
+    articleTitle.appendChild(articleDate)
+    articleDate.appendChild(articleContent)
+    articleContent.appendChild(articleExpand)
+
+
+
+}
